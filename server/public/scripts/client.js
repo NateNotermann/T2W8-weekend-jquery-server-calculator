447cf2830f6d34dab88d2a13a1f67a915b38eeaa
Nate Notermann <nate.notermann@gmail.com>
2022-06-25 16:31:30 -0500

    about to change if function and function order

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,30 +1,36 @@
 $(document).ready(onReady);
 
 function onReady(){
+    // ACTION 1. 
     console.log( 'js' );
     // $('#submitButton').on('click', submitMath ); // listener works
     $('#submitButton').on('click', inputValues ); // listener works
+
     $('#plusButton').on('click', plusPush ); // listener works
     $('#minusButton').on('click', minusPush );
     $('#divideButton').on('click', dividePush );
     $('#multiplyButton').on('click', multiplyPush );
+    $('#clearButton').on('click', clearButton );
+
      // $('main').on( 'click', '.deleteButton', deleteButton ); // example syntax
 }
 
-//global variable to save math button functions in a string
+// GLOBAL VARIABLES to save math button functions in a string
+// OPERATORS 
 let plus = '+';
 let minus ='-';
 let divide = '/';
 let multiply = '*';
 console.log( 'I like to ', plus, minus, divide, ' and',multiply); // tests variables
 
+// INPUTS & CURRENT SELECTED OPERATOR
 let input1 = 0;   // will be input 1's number
 let operator = '';
 let input2 = 0; // will be input 2's number
-console.log('test 2 inputs, before adding values', input1, input2 );
-// works 
-let math = 0;
+console.log('test 2 inputs, before adding values', input1, input2 ); // works 
 
+// EQUATION 
+let math = 0;
 
 function ifFunction (){ 
     if (operator === '' ){
@@ -51,9 +57,10 @@ function ifFunction (){
 
 function inputValues (){
     // console.log( 'button click' ); //test click listener
-
-    input1 = Number($('#firstNumber').val()); // takes input 1 value, converts itt to #
-    input2 = Number($('#secondNumber').val());// takes input 2 value, converts itt to #
+    
+    // turn off auto clear input funtion. Instructions say to do this in a button
+    // input1 = Number($('#firstNumber').val()); // takes input 1 value, converts itt to #
+    // input2 = Number($('#secondNumber').val());// takes input 2 value, converts itt to #
     ifFunction();
     // console.log('inputs after function', (input1 + input2) );
 
@@ -118,7 +125,7 @@ function multiplyPush (){ // function for multiply math
 
 
 
-function clearButton (){
+function clearButton (){  // works
 console.log( 'clear button');
 $('#firstNumber').val('');
 $('#secondNumber').val('');
