fb7ba3cefdf50d9f8e698faf07583b3e3d351ba4
Nate Notermann <nate.notermann@gmail.com>
2022-06-26 18:21:35 -0500

    got ver to start on port 5000

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-console.log( 'server.js' );
-
 // Imports
-const express = require('express');  
+// Require express - gives us a function
+const express = require('express'); 
 //it knows to look in the node_modules folder.
     // unless, if you give it a 'dot wack' aka './folder/folder', 
         //then it can look in a different folder path you created
@@ -13,9 +12,8 @@ const PORT = 5000;
 app.use(express.static('server/public'));
 
 //run the server, on the port we want
-app.listen(PORT, 
-    function() {
-        console.log( 'SERVER RUNNING ON PORT' );
-
-
-});
+// Start up our server
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+    console.log( 'SERVER RUNNING ON PORT' );
+  });
