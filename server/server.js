623c7cf2c3eddbb061ca78285a23c470e1995f38
Nate Notermann <nate.notermann@gmail.com>
2022-06-26 18:08:02 -0500

    didn;t complete intime -  working to finish. fixed gitignore

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+app.use(express.static('server/public'));
+
 //run the server, on the port we want
 app.listen(PORT, 
     function() {
