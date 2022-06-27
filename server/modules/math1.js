4bf474bc6d1016ae4b90c77ac682fda9abe14d66
Nate Notermann <nate.notermann@gmail.com>
2022-06-27 00:35:00 -0500

    get and post FINALLY works. Still need to move math logic to serveer file

---
+++ server/modules/math1.js
@@ -0,0 +1,20 @@
+// let moduleArrayOfMathObjects = [ // ARRAY of OBJECTS
+// { input1: 'example math 20',
+//   operator: '+', 
+//   input2: '5',
+//   math: '25', },
+
+//   { input1: 'example math 22',
+//   operator: '-', 
+//   input2: '6',
+//   math: '16', }
+// ];
+
+// console.log('math1.js is connected and moduleArrayOfMathObjects is',
+// moduleArrayOfMathObjects );
+// module.export = moduleArrayOfMathObjects;
+
+
+
+
+// ----------    COULDNT GET MODULE TO SEND THE OBJECT. IT ONLY SENT AN EMPTY OBJECT ---------
