a55b502f8d480c15252429a187482f44d7ae0971
Nate Notermann <nate.notermann@gmail.com>
2022-06-26 22:29:47 -0500

    POST function works, prints to SERVER terminal

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Imports
 // Require express - gives us a function
 const express = require('express'); 
+let bodyParser = require('body-parser');
 //it knows to look in the node_modules folder.
     // unless, if you give it a 'dot wack' aka './folder/folder', 
         //then it can look in a different folder path you created
@@ -9,19 +10,33 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+// our app.use functions
 app.use(express.static('server/public'));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 // Start up our server
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
-  });
+} );
 
 
 // http://localhost:5000/math1
-app.get('/math1', function(req, res) {
+app.get('/math1', function(req, res){  
     console.log('in the GET MATH1 function');
     res.send(serverMathArrayOfObjects)
 } );
+
+app.post('/math1', (req, res) =>  { // cant use 'arrow function' with jQuery or client.js
+    // where is our math?
+    console.log( 'POST /math1 start');
+    serverMathArrayOfObjects.push( req.body );
+    console.log('POST req-body is', req.body);
+    console.log('serverMathArrayOfObjects is:', serverMathArrayOfObjects );
+    res.sendStatus(201);
+    // save our math...
+//send back math
+} );
+
+
 
 
 
